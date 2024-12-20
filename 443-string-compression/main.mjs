6fcc0929eb59c1ffc6960899edbf8dc5df8f1b88
meynardtrinidad <meynard.trinidad44@gmail.com>
2024-12-20 20:44:30 +0800

    443

--- 443-string-compression/main.mjs
+++ 443-string-compression/main.mjs
@@ -1,50 +1,90 @@
 import assert from 'node:assert/strict';
 
 /**
- * FIXME: Dumb implementation
- *
  * @param {character[]} chars
- * @return {number}
+ * @return {characters[]}
  */
 const compress = function(chars) {
-  let str = ''
-  let curr = ''
-  let freq = 0
+  let i = 0
+  let ctr = 0
 
-  for (let i = 0; i < chars.length; i++) {
-    if (i == 0) {
-      str += chars[i]
-      curr = chars[i]
+  while (i < chars.length) {
+    let freq = 1
+    let j = i + 1
+
+    while (chars[i] == chars[j]) {
       freq++
-      continue
+      j++
     }
 
-    if (curr == chars[i]) {
-      freq++
-    } else {
-      if (freq > 1) {
-        str += freq
+    chars[ctr++] = chars[i]
+    if (freq > 1) {
+      let str = freq.toString()
+      for (let k = 0; k < str.length; k++) {
+        chars[ctr++] = str[k]
       }
-
-      curr = chars[i]
-      str += curr
-      freq = 1
     }
 
-    if (i == chars.length - 1 && freq > 1) {
-      str += freq
-    }
+    i += freq
   }
 
-  chars.splice(str.length)
-
-  for (let i = 0; i < str.length; i++) {
-    chars[i] = str[i]
-  }
-
-  return chars.length
+  chars.splice(ctr)
+  return chars
 };
 
-assert.equal(compress(["a", "a", "b", "b", "c", "c", "c"]), ["a", "2", "b", "2", "c", "3"].length)
-assert.equal(compress(["a"]), ["a"].length)
-assert.equal(compress(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]), ["a", "b", "1", "2"].length)
+// /**
+//  * FIXME: Dumb implementation
+//  *
+//  * @param {character[]} chars
+//  * @return {number}
+//  */
+// const compress = function(chars) {
+//   let str = ''
+//   let curr = ''
+//   let freq = 0
+// 
+//   for (let i = 0; i < chars.length; i++) {
+//     if (i == 0) {
+//       str += chars[i]
+//       curr = chars[i]
+//       freq++
+//       continue
+//     }
+// 
+//     if (curr == chars[i]) {
+//       freq++
+//     } else {
+//       if (freq > 1) {
+//         str += freq
+//       }
+// 
+//       curr = chars[i]
+//       str += curr
+//       freq = 1
+//     }
+// 
+//     if (i == chars.length - 1 && freq > 1) {
+//       str += freq
+//     }
+//   }
+// 
+//   chars.splice(str.length)
+// 
+//   for (let i = 0; i < str.length; i++) {
+//     chars[i] = str[i]
+//   }
+// 
+//   return chars.length
+// };
+
+const r1 = compress(["a", "a", "b", "b", "c", "c", "c"])
+assert.deepEqual(r1, ["a", "2", "b", "2", "c", "3"])
+assert.equal(r1.length, ["a", "2", "b", "2", "c", "3"].length)
+
+const r2 = compress(["a"])
+assert.deepEqual(r2, ["a"])
+assert.equal(r2.length, ["a"].length)
+
+const r3 = compress(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"])
+assert.deepEqual(r3, ["a", "b", "1", "2"])
+assert.equal(r3.length, ["a", "b", "1", "2"].length)
